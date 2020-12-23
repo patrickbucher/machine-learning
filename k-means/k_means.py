9d0ee5e1e3c9bcadfae881cece89d93ba39fee58
Patrick Bucher <patrick.bucher@mailbox.org>
2020-12-23 21:59:13 +0100

    k means: multiple initializations

--- k-means/k_means.py
+++ k-means/k_means.py
@@ -29,6 +29,17 @@ def find_closest(point, centroids):
     return min_index
 
 
+def calculate_cost(points, clusters, centroids):
+    total_distances = 0.0
+    m = len(points)
+    for i in range(m):
+        point = points[i] 
+        point_cluster = clusters[i][0]
+        centroid = centroids[point_cluster]
+        total_distances += euclidian_distance(point, centroid)
+    return total_distances / m
+
+
 def main():
     # read and reduce data
     athletes = pd.read_csv('athletes.csv')
@@ -36,24 +47,43 @@ def main():
 
     # K-means: initialize
     K = 5
+    initializations = 20
+    iterations = 1000
     m, n = bodies.shape
-    centroids = np.zeros((K, n))
-    rand_indices = np.random.randint(m, size=K)
-    centroids = bodies[rand_indices,:]
-    clusters = np.zeros((m, 1))
 
-    for _ in range(1000):
+    lowest_cost = 1e9
+    best_centroids = None
+    best_clusters = None
 
-        # step 1: cluster assignment by proximity
-        for i in range(m):
-            closest_index = find_closest(bodies[i,:], centroids)
-            clusters[i] = closest_index
+    for _ in range(initializations):
 
-        # step 2: move centroid
-        for k in range(K):
-            k_points = bodies[(clusters == k)[:,0]]
-            if len(k_points) > 0:
-                centroids[k] = k_points.sum(axis=0) / k_points.shape[0]
+        # random initialization
+        rand_indices = np.random.randint(m, size=K)
+        centroids = np.zeros((K, n))
+        centroids = bodies[rand_indices,:]
+        clusters = np.zeros((m, 1), dtype=int)
+
+        for _ in range(iterations):
+
+            # step 1: cluster assignment by proximity
+            for i in range(m):
+                closest_index = find_closest(bodies[i,:], centroids)
+                clusters[i] = closest_index
+
+            # step 2: move centroid
+            for k in range(K):
+                k_points = bodies[(clusters == k)[:,0]]
+                if len(k_points) > 0:
+                    centroids[k] = k_points.sum(axis=0) / k_points.shape[0]
+
+        cost = calculate_cost(bodies, clusters, centroids)
+        if cost < lowest_cost:
+            lowest_cost = cost
+            best_centroids = centroids
+            best_clusters = clusters
+
+    centroids = best_centroids
+    clusters = best_clusters
 
     # textual output
     for k in range(K):
@@ -70,11 +100,11 @@ def main():
         weight_y = points[:,1]
         color = colors(k)
         plt.scatter(height_x, weight_y, label=k+1, marker='o', color=color)
-        plt.scatter(centroids[k,0], centroids[k,1], marker='x', color=color)
+        plt.scatter(centroids[k,0], centroids[k,1], marker='x', s=128, color=color)
     plt.legend()
     plt.xlabel('height [m]')
     plt.ylabel('weight [kg]')
-    plt.savefig('clusters.png')
+    plt.savefig('clusters.svg')
     plt.show()
 
 

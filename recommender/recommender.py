ec2a9cec2cb6180cd6a63df68f9f811e01356ba1
Patrick Bucher <patrick.bucher@mailbox.org>
2020-12-28 17:03:02 +0100

    recommender: compute cost

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -3,6 +3,10 @@
 import numpy as np
 import pandas as pd
 
+
+def round_to(arr, granularity):
+    return (arr * 1/granularity).round() * granularity
+
 movies = pd.read_csv('movies.csv')
 ratings = movies.iloc[:,1:].to_numpy()
 
@@ -16,10 +20,10 @@ xs = np.random.rand(m, 3) # two "categories", by movie
 thetas = np.random.rand(n, 3) # no additional bias unit, by user
 thetas[:,0] = 1
 
-alpha = 1e-6
+alpha = 1e-4
 l = 1e-12
 ys = ratings
-for _ in range(500_000):
+for i in range(500_000):
     pred = xs.dot(thetas.transpose())
     pred[np.isnan(ratings)] = 0
 
@@ -32,6 +36,13 @@ for _ in range(500_000):
     xs -= alpha * xs_grad
     thetas -= alpha * thetas_grad
 
+    cost = (1/2) * (diff ** 2).sum() + (l/2) * (thetas ** 2).sum() + (l/2) * (xs ** 2).sum()
+    if cost < 1e-12:
+        print(f'finished with cost {cost} after {i} iterations')
+        break
+    last_cost = cost
+
 predictions = xs.dot(thetas.transpose())
 predictions[rated == 1] = 0
+predictions = round_to(predictions, 0.5)
 print(predictions)
